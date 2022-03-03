703175594fc3c51d38eb431f6ff27f67de8c43b7
Emerson Franciscoo <emersonairton@hotmail.com>
2022-03-02 21:19:41 -0300

    update folder pacotes with lib checkmail

--- Pacotes/main.go
+++ Pacotes/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/badoux/checkmail"
 	"modulo/auxiliar"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("herminhoo1@gmail.com")
+	fmt.Println(erro)
+
+	erro = checkmail.ValidateFormat("herminhoo1")
+	fmt.Println(erro)
 }
